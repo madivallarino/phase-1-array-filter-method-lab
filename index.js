08c70dffbb7574f3ec7e1ea892d1e1f6001b9cd8
Madison Vallarino <madivallarino@gmail.com>
2021-08-13 00:03:19 -0500

    commit tree 505827de79edf91815e9278613ee5611f6b5d58b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(driver, stuff){
+    return driver.filter ( possibleMatch => 
+        possibleMatch.toLowerCase()=== stuff.toLowerCase())
+}
+
+function fuzzyMatch(driver, moreStuff) {
+    return driver.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(moreStuff.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(driver, otherStuff) {
+    return driver.filter( record => record.name === otherStuff)
+  }
+
+  
